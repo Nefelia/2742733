280db243dbddb5e9ddefe583f2b08e762d4d2c3e
valeria <you@example.com>
2024-05-21 16:01:16 -0500

    Feat: SQL

---
+++ 07-SQL/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// 1. SELECT
+// SELECT Nombre, Grado FROM 'estudiantes';
+// Saca los datos de las columnas especificadas de la tabla "estudiantes"
+
+// 2. SELECT DISTINCT 
+// SELECT DISTINCT Grado FROM 'estudiantes';
+// Saca cada dato distinto de la columna especificada
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
