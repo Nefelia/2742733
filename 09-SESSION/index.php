8bb9d892a0c629b2396e3bb261b7f62752588c7f
valeria <you@example.com>
2024-06-06 15:52:03 -0500

    Feat: Inicio Sesión

--- 09-SESSION/index.php
+++ 09-SESSION/index.php
@@ -1,21 +1,20 @@
-<?php 
-
-session_start();
+<?php  session_start();
          
 if( $_SERVER["REQUEST_METHOD"] == 'POST' ){
 
-    $usuario = $_POST['user'];
+    $usuario = $_POST['username'];
     $password = $_POST['password'];
 
     $user_register = isset($_SESSION['userRegister']) ? $_SESSION['userRegister'] : null;
     $pass_register = isset($_SESSION['passRegister']) ? $_SESSION['passRegister'] : null;
 
     if( empty($usuario) or empty($password) ){
-        echo 'Rellene completo el formulario';
+        echo '<div class="mensaje">
+                <h5>Usuario o Contraseña incorrectos</h5>
+            </div>';
     } else {
         try {
             $conexion = new PDO('mysql: host=localhost; dbname=focaapp', 'root','');
-
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
         }
@@ -33,7 +32,9 @@ if( $_SERVER["REQUEST_METHOD"] == 'POST' ){
             header('location: user.php');
             
         } else {
-            echo 'Usuario o Contraseña incorrectos';
+            echo '<div class="mensaje">
+                    <h5>Usuario o Contraseña incorrectos</h5>
+                </div>';
         }
     }
 }
@@ -45,19 +46,40 @@ if( $_SERVER["REQUEST_METHOD"] == 'POST' ){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
+
 <body>
-<h1>Página de inicio</h1>
-
-    <form action="index.php" method="POST">
-        <label for="user">User</label>
-        <input type="text" placeholder="User" name="user">
-        <label for="password">Password</label>
-        <input type="password" placeholder="Password" name="password">
-        <button type="submit">Inicio sesión</button>
-    </form>
-
-    <a href="./registro.php">Registrate</a>
-
-</body>
+    <div class="content container-fluid">
+        <div class="row">
+            <div class="col text-center fondo">
+                <div class="title">
+                    <h1>TOP TEXT <br> BOTTOM <br> TEXT</h1>
+                </div>
+            </div>
+            <div class="tarjeta text-center col justify-content-end">
+                <div class="tarjeta-header">
+                    <ul class="nav nav-fill nav-tabs border-0">
+                        <li class="nav-item inactive">
+                            <a href="./registro.php" class="nav-link" style="color: rgba(224, 70, 106, 0.4); font-weight:bold; font-size:large;">Registro</a>
+                        </li>
+                        <li class="nav-item inactive">
+                            <a href="" class="nav-link active" aria-current="true" style="color: #e0466b; font-weight:bold; font-size:large;">Iniciar Sesión</a>
+                        </li>
+                    </ul>
+                </div>
+                <h1>INICIAR SESIÓN</h1>
+                <form action="index.php" method="POST" class="formulario formulario2">
+                    <i class="icono fa-solid fa-user fa-xl" style="color: #e0466b;"></i>
+                    <input type="username" id="username" placeholder="nombre de usuario" name="username" require>
+                    <i class="icono fa-solid fa-lock fa-xl" style="color: #e0466b;"></i>
+                    <input type="password" id="password" placeholder="contraseña" name="password" require>
+                    <button type="submit" class="boton boton2">Iniciar Sesión</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </html>
--- 09-SESSION/registro.php
+++ 09-SESSION/registro.php
@@ -1,18 +1,18 @@
 <?php session_start();
 
-
-
-if($_SERVER['REQUEST_METHOD']== 'POST') {
-    $usuario = $_POST['user'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $usuario = $_POST['username'];
     $password = $_POST['password'];
     $email = $_POST['email'];
+    $confirm = $_POST['confirm'];
 
-    if( empty($usuario) or empty($password)){
-        echo 'Rellene completo el formulario';
-    } else{
+    if (empty($usuario) or empty($password) or $password !== $confirm) {
+        echo '<div class="mensaje">
+                <h5>Usuario o Contraseña incorrectos</h5>
+            </div>';
+    } else {
         try {
-            $conexion = new PDO('mysql: host=localhost; dbname=focaapp', 'root','');
-        
+            $conexion = new PDO('mysql: host=localhost; dbname=focaapp', 'root', '');
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
         }
@@ -20,14 +20,12 @@ if($_SERVER['REQUEST_METHOD']== 'POST') {
         $statement = $conexion->prepare("INSERT INTO `usersapp`(`ID`, `username`, `correo`, `contraseña`) 
         VALUES (NULL, :username , :email, :pass)");
 
-        $statement-> execute(array(":username"=>$usuario, ":email"=>$email, ":pass"=>$password));
+        $statement->execute(array(":username" => $usuario, ":email" => $email, ":pass" => $password));
 
-        $statement = $statement ->fetchAll();
+        $statement = $statement->fetchAll();
 
-        header("location: index.php");
+        header('location: index.php');
     }
-
-    echo 'Te has registrado';
 }
 
 ?>
@@ -39,27 +37,48 @@ if($_SERVER['REQUEST_METHOD']== 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 
 <body>
-<h1>Registrate</h1>
+    <div class="content container-fluid">
+        <div class="row">
+            <div class="col text-center fondo">
+                <div class="title">
+                    <h1>TOP TEXT <br> BOTTOM <br> TEXT</h1>
+                </div>
+            </div>
+            <div class="tarjeta text-center col justify-content-end">
+                <div class="tarjeta-header">
+                    <ul class="nav nav-fill nav-tabs border-0">
+                        <li class="nav-item inactive">
+                            <a href="" class="nav-link active" aria-current="true" style="color: #e0466b; font-weight:bold; font-size:large;">Registro</a>
+                        </li>
+                        <li class="nav-item inactive">
+                            <a href="./index.php" class="nav-link" style="color: rgba(224, 70, 106, 0.4); font-weight:bold; font-size:large;">Iniciar Sesión</a>
+                        </li>
+                    </ul>
+                </div>
+                <h1>REGISTRARSE</h1>
+                <form action="registro.php" method="POST" class="formulario">
+                    <i class="icono fa-solid fa-user fa-xl" style="color: #e0466b;"></i>
+                    <input type="username" id="username" placeholder="nombre de usuario" name="username" require>
+                    <i class="icono fa-solid fa-envelope fa-xl" style="color: #e0466b;"></i>
+                    <input type="email" id="email" placeholder="correo" name="email" require>
+                    <i class="icono fa-solid fa-lock fa-xl" style="color: #e0466b;"></i>
+                    <input type="password" id="password" placeholder="contraseña" name="password" require>
+                    <i class="icono fa-solid fa-lock fa-xl" style="color: #e0466b;"></i>
+                    <input type="password" id="confirm" placeholder="confirmar contraseña" name="confirm" require>
+                    <button type="submit" class="boton">Registro</button>
+                </form>
+            </div>
+        </div>
+    </div>
 
-    <form action="registro.php" method="POST">
-        <label for="email">Email</label>
-        <input type="email" id="email" placeholder="Email" name="email" required> 
-        <label for="user">User</label>
-        <input type="text" placeholder="User" name="user">
-        <label for="password">Password</label>
-        <input type="password" placeholder="Password" name="password" required>
-        <button type="submit">Registrarse</button>
-    </form>
-
-    <?php if( isset($_SESSION['userRegister']) ) : ?>
-        <p>Datos registrados, ya puedes iniciar sesión</p>
-        <p> <?php echo $_SESSION['userRegister'] . ' - ' . $_SESSION['passRegister'] . '<br>' . $_SESSION['emailRegister']; ?></p>
-        <a href="index.php">Iniciar sesión</a>
-    <?php endif ?>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
