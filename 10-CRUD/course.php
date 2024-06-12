f1360679daf2916526b94d14a1a5871f29077490
valeria <you@example.com>
2024-06-12 15:19:15 -0500

    commit

---
+++ 10-CRUD/course.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style/course.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"/>
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <div class="container-fluid text-center">
+            <div class="row header">
+                <div class="col-1">
+                    <div class="logo-header">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form">
+                        <input type="search" class="form-input search" placeholder="BUSCAR...">
+                    </div>
+                </div>
+                <div class="col-6 usuario">
+                    <a href="" class="inicio">INICIAR SESIÓN</a>
+                    <a href="" class="registro">REGISTRARSE</a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container-fluid text-center">
+            <div class="row footer">
+                <div class="col-4 copyright">
+                    <p>Copyright</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia tempora modi optio provident,
+                        eum vero qui quam impedit quos, consequuntur libero nam beatae. Repellendus eos minus maiores
+                        illo deserunt eius.</p>
+                </div>
+                <div class="row row-cols-2 col-6 redes">
+                    <div>
+                        <div class="x">
+                            <i class="fa-brands fa-x-twitter ix" style="color: #e41476;"></i>
+                            <p>@musica123</p>
+                        </div>
+                        <div class="inst">
+                            <i class="fa-brands fa-instagram iinst" style="color: #e41476;"></i>
+                            <p>musica123</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <div class="google">
+                                <i class="fa-brands fa-google igoogle" style="color: #e41476;"></i>
+                                <p>musica123@gmail.com</p>
+                            </div>
+                            <div class="wsp">
+                                <i class="fa-brands fa-whatsapp iwsp" style="color: #e41476;"></i>
+                                <p>(##)123-456 123</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="logo-footer"></div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
---
+++ 10-CRUD/user.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style/user.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <div class="container-fluid text-center">
+            <div class="row header">
+                <div class="col-1">
+                    <div class="logo-header">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form">
+                        <input type="text" class="form-input search" placeholder="BUSCAR...">
+                    </div>
+                </div>
+                <div class="col-6 usuario">
+                    <a href="" class="inicio">INICIAR SESIÓN</a>
+                    <a href="" class="registro">REGISTRARSE</a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Body -->
+    <section>
+        <div class="container-fluid">
+            <div class="row">
+                <a href="">Crear Curso</a>
+                <a href="">Mis Cursos</a>
+            </div>
+            <div class="row">
+                <h2>Crear Curso</h2>
+                <div class="col">
+                    <form action="">
+                        <input type="text">
+                    </form>
+                </div>
+                <div class="col">
+                    <form action="">
+                        input
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <table class="table table-bordered border-primary">
+        <thead>
+            <tr class="tr">
+
+                <th scope="col">ID</th>
+                <th scope="col">Estudiantes</th>
+                <th scope="col">Imagen</th>
+                <th scope="col">Descripicion</th>
+                <th scope="col">Curso</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
