04eb8b0424a770fe1aab8194fb6cbbd44a8bd92e
Nefelia <148262707+Nefelia@users.noreply.github.com>
2024-06-27 23:15:27 -0500

    Delete php-2742733-main directory

--- php-2742733-main/05-include-require/components/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <div class="container-fluid">
-        <div class="row text-center">
-            <p style="background-color: black; color: white; padding: 10px 0px 10px 0px;">Todos los derechos reservados
-            </p>
-        </div>
-    </div>
-</footer>
